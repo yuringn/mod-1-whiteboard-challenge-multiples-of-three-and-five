3ccd908739074501b31b230ad97fa2de7a669dc1
Yuri Nguyen <yuringuyen312@gmail.com>
2021-03-11 10:24:46 -0500

    commit tree 95329ce13ef8439e601904dc6633b4a16f2c2139

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    (1...num).find_all{|i| i%3==0 ||i%5==0}
+end
+
+def sum_multiples(num)
+    collect_multiples(num).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+    attr_accessor :num
+
+def initialize(num)
+    @num = num
+end
+
+def collect_multiples
+    (1...num).find_all{|i| i%3==0 ||i%5==0}
+end
+
+def sum_multiples
+    collect_multiples.sum
+end
+end
